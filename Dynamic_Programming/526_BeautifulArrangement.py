ce628b5057b96a00836cca1422e7671f58be78f4
ahlee-shawn <ahlee.shawn@gmail.com>
2021-01-04 09:26:33 +0800

    Create 526_BeautifulArrangement.py

---
+++ Dynamic_Programming/526_BeautifulArrangement.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countArrangement(self, n: int) -> int:
+        def dfs(bm, pl):
+            if pl == 0: return 1
+                
+            S = 0
+            for i in range(n):
+                if not bm&1<<i and ((i+1)%pl == 0 or pl%(i+1) == 0):
+                    S += dfs(bm^1<<i, pl - 1)
+            return S
+                
+        return dfs(0, n)
