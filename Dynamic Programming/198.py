656ca7abdfc75929e709d14c4dc228e7d3bef937
ahlee-shawn <ahlee.shawn@gmail.com>
2021-05-11 21:20:00 +0800

    Update 198

--- Dynamic Programming/198.py
+++ Dynamic Programming/198.py
@@ -7,3 +7,14 @@ class Solution:
         for i in range(2, len(nums)):
             dp[i] = max(dp[i-1], dp[i-2] + nums[i])
         return max(dp)
+
+
+#         2 1 1 2
+# last    0 2 2 3
+# now     2 2 3 4
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        last, now = 0, 0
+        for num in nums:
+            last, now = now, max(last+num, now)
+        return now
