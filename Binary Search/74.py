cb9cba0ab5af454f415eeb8ab79743cde2f35898
ahlee-shawn <ahlee.shawn@gmail.com>
2021-05-13 13:44:55 +0800

    Update 74

--- Binary Search/74.py
+++ Binary Search/74.py
@@ -3,12 +3,11 @@ class Solution:
         m, n = len(matrix), len(matrix[0])
         start, end = 0, m*n-1
         while start <= end:
-            if matrix[(start-1) // n][(start-1) % n] == target or matrix[(end-1) // n][(end-1) % n] == target:
-                return True
             mid = start + ((end - start) // 2)
-            if matrix[(mid-1) // n][(mid-1) % n] > target:
+            num = matrix[mid // n][mid % n]
+            if num > target:
                 end = mid - 1
-            elif matrix[(mid-1) // n][(mid-1) % n] < target:
+            elif num < target:
                 start = mid + 1
             else:
                 return True
