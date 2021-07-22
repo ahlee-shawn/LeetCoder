8328a6d0b7abece361e99bee2ff271fd7d334a19
李昂軒(Ang-Hsuan Lee) <ahlee.shawn@gmail.com>
2021-07-22 15:37:07 +0800

    Update 706.py

--- Hash Table/706.py
+++ Hash Table/706.py
@@ -1,3 +1,78 @@
+class Node:
+    def __init__(self):
+        self.key = 0
+        self.val = 0
+        self.next = None
+
+class MyHashMap:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.mod = 1000
+        self.table = [None for _ in range(self.mod)]
+
+    def put(self, key: int, value: int) -> None:
+        """
+        value will always be non-negative.
+        """
+        if self.table[key % self.mod] is None:
+            newNode = Node()
+            newNode.key = key
+            newNode.val = value
+            self.table[key % self.mod] = newNode
+        else:
+            curNode = self.table[key % self.mod]
+            while curNode:
+                if curNode.key == key:
+                    curNode.val = value
+                    return
+                curNode = curNode.next
+            newNode = Node()
+            newNode.key = key
+            newNode.val = value
+            newNode.next = self.table[key % self.mod]
+            self.table[key % self.mod] = newNode
+
+    def get(self, key: int) -> int:
+        """
+        Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
+        """
+        if self.table[key % self.mod] is None:
+            return -1
+        else:
+            curNode = self.table[key % self.mod]
+            while curNode:
+                if curNode.key == key:
+                    return curNode.val
+                curNode = curNode.next
+            return -1
+
+    def remove(self, key: int) -> None:
+        """
+        Removes the mapping of the specified value key if this map contains a mapping for the key
+        """
+        if self.table[key % self.mod] is not None:
+            curNode = self.table[key % self.mod]
+            if curNode.key == key:
+                self.table[key % self.mod] = curNode.next
+                return
+            prevNode = curNode
+            curNode = curNode.next
+            while curNode:
+                if curNode.key == key:
+                    prevNode.next = curNode.next
+                prevNode = curNode
+                curNode = curNode.next
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
+
 class MyHashMap:
 
     def __init__(self):
