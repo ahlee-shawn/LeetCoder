a4e8e8613a4cc9444954333b0dc6cad1ae9cefa0
李昂軒(Ang-Hsuan Lee) <ahlee.shawn@gmail.com>
2021-08-26 18:19:34 -0400

    Update 1710

--- Greedy/1710.py
+++ Greedy/1710.py
@@ -10,3 +10,18 @@ class Solution:
                 ans += (truckSize * boxTypes[i][1])
                 break
         return ans
+
+class Solution:
+    def maximumUnits(self, boxTypes: List[List[int]], truckSize: int) -> int:
+        ans = 0
+        boxTypes.sort(key = lambda x: x[1], reverse=True)
+        for i in range(len(boxTypes)):
+            if boxTypes[i][0] <= truckSize:
+                ans += (boxTypes[i][0] * boxTypes[i][1])
+                truckSize -= boxTypes[i][0]
+            else:
+                ans += (truckSize * boxTypes[i][1])
+                break
+            if truckSize == 0:
+                break
+        return ans
