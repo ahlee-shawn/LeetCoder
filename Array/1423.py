c7148c825104dcfec432c3c0f88d0d097ef91ebc
李昂軒(Ang-Hsuan Lee) <ahlee.shawn@gmail.com>
2021-09-09 15:03:54 -0400

    Update 1423.py

--- Array/1423.py
+++ Array/1423.py
@@ -12,3 +12,26 @@ class Solution:
             left_index -= 1
             right_index -= 1
         return ans
+
+class Solution:
+    def maxScore(self, cardPoints: List[int], k: int) -> int:
+        n = len(cardPoints)
+        if k == n:
+            return sum(cardPoints)
+        forward = [0 for _ in range(n)]
+        backward = [0 for _ in range(n)]
+        temp = 0
+        for i in range(k):
+            temp += cardPoints[i]
+            forward[i] = temp
+        temp = 0
+        for i in range(k):
+            temp += cardPoints[n - 1 - i]
+            backward[n - 1 - i] = temp
+        ans = max(forward[k - 1], backward[n - k])
+        left, right = 0, n - k + 1
+        for i in range(k - 1):
+            ans = max(ans, forward[left] + backward[right])
+            left += 1
+            right += 1
+        return ans
