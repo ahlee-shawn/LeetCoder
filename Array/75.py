7edf4b69253e8f4a3d9cfe7ffdcdebb37bda1697
李昂軒(Ang-Hsuan Lee) <ahlee.shawn@gmail.com>
2021-09-10 11:33:07 -0400

    Update 75.py

--- Array/75.py
+++ Array/75.py
@@ -1,3 +1,21 @@
+# One pass with constant space
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        i, left, right = 0, 0, len(nums) - 1
+        while i <= right:
+            if nums[i] == 0:
+                nums[i], nums[left] = nums[left], nums[i]
+                left += 1
+                i += 1
+            elif nums[i] == 2:
+                nums[i], nums[right] = nums[right], nums[i]
+                right -= 1
+            else:
+                i += 1
+
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
         """
